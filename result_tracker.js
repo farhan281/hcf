f5c5bfb2bd3dad154265399a2a7680c011d83798
farhan281 <farhan.ansari@perceptionsystem.in>
2026-04-07 12:09:36 +0530

    auto: 2026-04-07T06:39:36.064Z

--- result_tracker.js
+++ result_tracker.js
@@ -5,7 +5,7 @@ const fs   = require('fs');
 const path = require('path');
 const { CSV_FIELDS, CSV_PATH, PROGRESS_FILE, OUTPUT_DIR } = require('./config');
 
-const SHEETS_URL = process.env.GOOGLE_SHEETS_URL || 'https://script.google.com/macros/s/AKfycbxrirb17CXY4T1s_uvK-m9p29S-PrfIs8L4CZPCfVXCg8NwMZz0XMVs9scuKJiNhBFX/exec';
+const SHEETS_URL = process.env.GOOGLE_SHEETS_URL || 'https://script.google.com/macros/s/AKfycbwZFu9gSUV0yo571sUsTKQtK4Gmnis9icSX1m6WF7JtVLcMgmgOPyiXTdMpkVnWwdyzuQ/exec';
 
 function sendToSheets(record) {
   if (!SHEETS_URL) return;
